c98238dbbf37484a65cd506ef4f33e6016fd4a86
Yamamoto Takashi <yamamoto.takashi@eiwa.com>
2015-10-27 23:22:39 +0900

    プロジェクトを追加

---
+++ Practice_Yamamoto/src/Java001/Test01.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+
+package Java001;
+
+/**
+ * コンソール表示 <br />
+ * コンソールに表示する <br />
+ * 更新履歴 yyyy/mm/dd （更新者）：新規作成 <br />
+ */
+public class Test01 {
+
+  /**
+   * メインメソッド 処理を起動します。 <br />
+   * コンソールに表示する <br />
+   *
+   * @param args 実行時引数
+   */
+  public static void main( String[] args ) {
+
+    // 次の文字列が１度の実行で表示される
+    System.out.println( "文字列の表示テスト。 " );
+
+  }
+}
