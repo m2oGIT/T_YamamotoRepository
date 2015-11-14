0f2cb6122aa3956de17645ed2aeae7169491c106
Yamamoto Takashi <yamamoto.takashi@eiwa.com>
2015-11-14 16:53:34 +0900

    初回コミットです。

---
+++ Practice_Yamamoto_t/src/Java001/Test01.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+
+package Java001;
+
+/**
+ * @author （作成者） <br />
+ *         （クラス論理名） <br />
+ *         （説明） <br />
+ *         更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test01 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * 
+   * @param args 起動引数
+   */
+  public static void main( String[] args ) {
+
+    System.out.println( "Hello World!!" );
+    System.out.println( args[0] );
+
+  }
+
+}
