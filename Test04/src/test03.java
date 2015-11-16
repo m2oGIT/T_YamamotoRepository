da7aabea556f12da133676ed7a5728eacb7ee5e1
Yamamoto Takashi <yamamoto.takashi@eiwa.com>
2015-11-17 00:48:18 +0900

    コミットテスト（64_4）

---
+++ Test04/src/test03.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author takashi
+ *
+ */
+public class test03 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
