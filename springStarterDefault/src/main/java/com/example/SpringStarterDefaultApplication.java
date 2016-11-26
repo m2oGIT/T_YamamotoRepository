0e06a3aecc51b1624654f2d6470bf8ef74a7e004
Yamamoto Takashi <yamamoto.takashi@eiwa.com>
2016-11-27 08:22:47 +0900

    文字コードのテスト（CSS）

---
+++ springStarterDefault/src/main/java/com/example/SpringStarterDefaultApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterDefaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterDefaultApplication.class, args);
+	}
+}
